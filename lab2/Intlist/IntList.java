46c456ba2792b9e3db17da8d2ce968163f0bd63f
tuyen <hnt4499@gmail.com>
2019-01-01 15:01:08 +0800

    Implemented catenate (1 test passed)

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -94,8 +94,10 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A.rest == null) {
+            return new IntList(A.first, B);
+        }
+        return new IntList(A.first, catenate(A.rest, B));
     }
 
 
