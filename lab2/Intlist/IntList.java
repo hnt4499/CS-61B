fd02fdd5b78fbbb4d1e4ee8957f6290cbe18b9b7
tuyen <hnt4499@gmail.com>
2019-01-01 14:55:02 +0800

    Implemented dCatenate (1 test passed)

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,8 +81,12 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A.rest == null) {
+            A.rest = B;
+            return A;
+        }
+        dcatenate(A.rest, B);
+        return A;
     }
 
     /**
