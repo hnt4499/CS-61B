fe935e68f2e477b8a64907202b9b130bf653fe24
tuyen <hnt4499@gmail.com>
2019-01-02 21:10:27 +0800

    - Implemented calcNetForceExertedByX and Y methods - Fixed calForceExertedByX and Y so that it can take the same target body.

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -32,10 +32,62 @@ public class Planet {
 
     /** Calculate the force exerted by a body in x and y planes, respectively. */
     public double calcForceExertedByX(Planet p) {
+        if (this.calcDistance(p) == 0) {
+            return 0;
+        }
         return this.calcForceExertedBy(p) * (p.xxPos - this.xxPos) / this.calcDistance(p);
     }
 
     public double calcForceExertedByY(Planet p) {
+        if (this.calcDistance(p) == 0) {
+            return 0;
+        }
         return this.calcForceExertedBy(p) * (p.yyPos - this.yyPos) / this.calcDistance(p);
     }
+
+    /** Calculate the NET force exerted by multiple bodies in x and y planes, respectively. */
+    public double calcNetForceExertedByX(Planet[] p) {
+        double res = 0; int count = 0;
+        while (count < p.length) {
+            res += this.calcForceExertedByX(p[count]);
+            count ++;
+        }
+        return res;
+    }
+
+    public double calcNetForceExertedByY(Planet[] p) {
+        double res = 0;
+        int count = 0;
+        while (count < p.length) {
+            res += this.calcForceExertedByY(p[count]);
+            count++;
+        }
+        return res;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
