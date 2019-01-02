10372a97912ab5c69ecff3e13ebcbc513adcc4c1
tuyen <hnt4499@gmail.com>
2019-01-02 20:30:42 +0800

    Initialized Planet class with two constructors

---
+++ proj0/Planet.java
@@ -0,0 +1,21 @@
+public class Planet {
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+
+    public Planet(double xxPos, double yyPos, double xxVel, double yyVel, double mass, String imgFileName) {
+        this.xxPos = xxPos;
+        this.yyPos = yyPos;
+        this.xxVel = xxVel;
+        this.yyVel = yyVel;
+        this.mass = mass;
+        this.imgFileName = imgFileName;
+    }
+
+    public Planet(Planet p) {
+        this(p.xxPos, p.yyPos, p.xxVel, p.yyVel, p.mass, p.imgFileName);
+    }
+}
